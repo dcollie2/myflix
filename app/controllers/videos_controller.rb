2e8253cee0f9717e2cf5c267421fc7c103d57827
Danny Collier <dcollie2@gmu.edu>
2014-06-05 18:29:39 -0400

    Show all categories on home.

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,6 @@
 class VideosController < ApplicationController
   def index
-    @videos = Video.all
+    @categories = Category.all.order(:label)
   end
 
   def show
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ActiveRecord::Base
   has_many :videos
+  
+  def has_no_videos?
+    videos.count < 1
+  end
 end
