b066d482df589c7bfc25c75ab8add6080ccb2195
Danny Collier <dcollie2@gmu.edu>
2014-06-11 14:41:12 -0400

    Implement recent videos.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,10 @@
 class Category < ActiveRecord::Base
-  has_many :videos, -> { order(:title) }
+  has_many :videos, -> { order("created_at DESC") }
   validates_presence_of :label
+  
+  def recent_videos
+    videos.first(6)
+  end
   
   def has_no_videos?
     videos.count < 1
